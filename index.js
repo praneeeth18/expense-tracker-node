530d95cafc4549c4486977ffdeb6ff6766babb7a
Praneeth Reddy <praneeth608@gmail.com>
2024-10-18 16:11:39 +0530

    MongoDB connection added

--- index.js
+++ index.js
@@ -1,4 +1,19 @@
-const express = require('express')
-const app = express()
+require('dotenv').config();
+const express = require('express');
+const mongoose = require('mongoose');
+const connectDB = require('./config/dbConnection.js');
+const app = express();
+const PORT = process.env.PORT || 3500;
 
+//dbconnection
+connectDB();
+
+//middleware
+app.use(express.json());
+app.use(express.urlencoded({extended: false}));
+
+mongoose.connection.once('open', () => {
+    console.log('Connected to MongoDB');
+    app.listen(PORT, () => console.log(`Server running at PORT: ${PORT}.`));
+});
 
